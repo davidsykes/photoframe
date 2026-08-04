ef12f1197fb336325a8cd1bb0409ce24421fc926
David Sykes <david@sykes.org.uk>
2026-08-04 07:54:11 +0100

    Implement the next simplest possible image selector

--- viewer/src/next_image_selector.py
+++ viewer/src/next_image_selector.py
@@ -1,9 +1,16 @@
 class NextImageSelector:
     def __init__(self, randomiser):
-        self.randomiser = randomiser
+        self._randomiser = randomiser
+        self._image_count = 0
+        self._current_image = 0
 
     def set_images(self, image_paths):
         self._image_paths = image_paths
+        self._image_count = len(image_paths)
+        self._current_image = -1
 
     def select_next_image(self):
-        return self._image_paths[0]
+        self._current_image += 1
+        if self._current_image >= self._image_count:
+            self._current_image = 0
+        return self._image_paths[self._current_image]
