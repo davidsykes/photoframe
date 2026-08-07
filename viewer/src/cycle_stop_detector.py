81fd733668369a053e7b9679634841fd1d9b892a
David Sykes <david@sykes.org.uk>
2026-08-07 06:28:10 +0100

    Implement the stop detector

--- viewer/src/cycle_stop_detector.py
+++ viewer/src/cycle_stop_detector.py
@@ -1,9 +1,14 @@
 class CycleStopDetector:
-    def __init__(self):
+    def __init__(self, reasons):
+        self._reasons = reasons
         self._stop = False
 
     def should_stop(self):
-        return self._stop
+        for reason in self._reasons:
+            if reason.should_stop():
+                self._stop = True
+                return True
+        return False
 
     def stop(self):
         self._stop = True
---
+++ viewer/tests/test_cycle_stop_detector.py
@@ -0,0 +1,45 @@
+import unittest
+from unittest.mock import Mock, call
+
+from viewer.src.cycle_stop_detector import CycleStopDetector
+
+
+class CycleStopDetectorTests(unittest.TestCase):
+    def test_no_stop_if_nothing_wants_to(self):
+        stop = self.out.should_stop()
+
+        self.assertFalse(stop)
+        
+    def test_stop_if_1_wants_to(self):
+        self.reason1.should_stop.return_value = True
+
+        stop = self.out.should_stop()
+
+        self.assertTrue(stop)
+        
+    def test_stop_if_2_wants_to(self):
+        self.reason2.should_stop.return_value = True
+
+        stop = self.out.should_stop()
+
+        self.assertTrue(stop)
+        
+    def test_stop_if_3_wants_to(self):
+        self.reason3.should_stop.return_value = True
+
+        stop = self.out.should_stop()
+
+        self.assertTrue(stop)
+
+    @classmethod
+    def setUp(self):
+        self.reason1 = Mock()
+        self.reason1.should_stop.return_value = False
+        self.reason2 = Mock()
+        self.reason2.should_stop.return_value = False
+        self.reason3 = Mock()
+        self.reason3.should_stop.return_value = False
+        self.out = CycleStopDetector(
+            [self.reason1,
+             self.reason2,
+             self.reason3])
