1162595fab27eefd74152860755f873c10372a3a
David Sykes <david@sykes.org.uk>
2026-08-08 06:41:56 +0100

    Fix exception error

--- viewer/src/viewer_exit_exception.py
+++ viewer/src/viewer_exit_exception.py
@@ -1,8 +1,8 @@
 class ViewerExitException(Exception):
-    def __init__(self, return_code, msg):
-        self.return_code = return_code
+    def __init__(self, exit_code, msg):
+        self.exit_code = exit_code
         self.msg = msg
         super().__init__(self.msg)
 
     def __str__(self):
-        return f'{self.return_code} -> {self.msg}'
+        return f'{self.exit_code} -> {self.msg}'
