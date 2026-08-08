abce35ea78f2e1e3bfa8c8df2fa94bc5b83adef7
David Sykes <david@sykes.org.uk>
2026-08-08 08:00:06 +0100

    Add a viewer config that gets updated with the app

--- viewer/src/photo_frame_app.py
+++ viewer/src/photo_frame_app.py
@@ -2,16 +2,16 @@ from enum import Enum, auto
 
 from common.src.config_file_updater import ConfigFileUpdater
 from common.src.remote_files_retriever import RemoteFilesRetriever
+from common.src.config_file_loader import ConfigFileLoader
 from viewer.src.cycle_stop_detector import CycleStopDetector
 from viewer.src.imagepathloader import ImagePathLoader
 from viewer.src.new_app_or_new_photos_detector import NewAppOrNewPhotosDetector
 from viewer.src.next_image_selector import NextImageSelector
 from viewer.src.randomiser import Randomiser
 from viewer.src.image_cycler import ImageCycler
-from common.src.config_file_loader import ConfigFileLoader
 from viewer.src.remote_config_version_loader import RemoteConfigVersionLoader
 from viewer.src.status_updater import StatusUpdater
-
+from viewer.src.action_timer import ActionTimer
 
 class DisplayType(Enum):
     PC_TEST_VERSION = auto()
@@ -22,15 +22,19 @@ class PhotoFrameApp:
         self._display_type = display_type
 
     def run(self, system_operations, PROJECT_ROOT):
-        ini_file_name = "project_config.json"
         config_file_loader = ConfigFileLoader(system_operations)
-        config_file = config_file_loader.load_config_file(ini_file_name)
-        image_path_path = config_file.get("images_folder")
+        whole_project_config_file_name = "project_config.json"
+        whole_project_configuration = config_file_loader.load_config_file(whole_project_config_file_name)
+        viewer_config_file_name = "viewer/viewer_config.json"
+        viewer_configuration = config_file_loader.load_config_file(viewer_config_file_name)
+        if viewer_configuration is None:
+            return 1
+        image_path_path = whole_project_configuration.get("images_folder")
         image_path_loader = ImagePathLoader(image_path_path)
-        sleep_time_seconds = config_file.get("sleep_time_seconds")
+        sleep_time_seconds = whole_project_configuration.get("sleep_time_seconds")
         randomiser = Randomiser()
         next_image_selector = NextImageSelector(randomiser)
-        remote_config_url = config_file.get("remote_config_url")
+        remote_config_url = whole_project_configuration.get("remote_config_url")
         remote_files_retriever = RemoteFilesRetriever(system_operations)
         config_file_updater = ConfigFileUpdater(
             remote_files_retriever,
@@ -42,13 +46,17 @@ class PhotoFrameApp:
             config_file_loader,
             status_updater,
             remote_config_url,
-            PROJECT_ROOT / 'viewer_config.json',
+            PROJECT_ROOT / 'remote_viewer_config.json',
             )
         new_app_or_new_photos_detector = NewAppOrNewPhotosDetector(
             remote_config_version_loader
         )
+        time_between_version_checks_seconds = viewer_configuration.get("time_between_version_checks_seconds")
+        timed_new_app_or_new_photos_detector = ActionTimer(
+            new_app_or_new_photos_detector.poll,
+            time_between_version_checks_seconds)
         cycle_stop_detector = CycleStopDetector(
-            [new_app_or_new_photos_detector]
+            [timed_new_app_or_new_photos_detector]
         )
 
         display = None
