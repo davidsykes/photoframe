8abb785019ebd2d749c1ea339bb3a433bf5aa95d
David Sykes <david@sykes.org.uk>
2026-08-12 06:58:24 +0100

    Sleeper handles ui events

--- viewer/src/sleeper.py
+++ viewer/src/sleeper.py
@@ -3,12 +3,20 @@
 class Sleeper:
     def __init__(self,
                  system_operations,
+                 display,
+                 event_handler,
                  sleep_time):
         self._system_operations = system_operations
+        self._display = display
+        self._event_handler = event_handler
         self._sleep_time = sleep_time
 
     def sleep(self):
         time_sec = self._system_operations.time()
         while self._system_operations.time() - time_sec < self._sleep_time:
-        #    self._handle_events()
+            self._handle_events()
             self._system_operations.sleep(0.1)
+
+    def _handle_events(self):
+        events = self._display.get_events()
+        self._event_handler.handle_events(events)
--- viewer/tests/test_sleeper.py
+++ viewer/tests/test_sleeper.py
@@ -6,9 +6,6 @@ from viewer.src.sleeper import Sleeper
 
 class SleeperTests(unittest.TestCase):
     def test_sleep_until_the_sleep_time_has_elapsed(self):
-        self.system_operations.time.side_effect = [
-            100, 101, 110, 125, 142]
-
         self.out.sleep()
 
         self.assertEqual(
@@ -18,12 +15,27 @@ class SleeperTests(unittest.TestCase):
             [call(0.1),call(0.1),call(0.1)]
         )
 
+    def test_while_sleeping_the_event_handlers_are_called(self):
+        self.display.get_events.side_effect = [
+            'one', 'two', 'three']
+
+        self.out.sleep()
+        
+        self.event_handler.handle_events.assert_has_calls(
+            [call('one'),call('two'),call('three')]
+        )
 
     @classmethod
     def setUp(self):
         self.system_operations = Mock()
+        self.system_operations.time.side_effect = [
+            100, 101, 110, 125, 142]
+        self.display = Mock()
+        self.event_handler = Mock()
         self.out = Sleeper(
             self.system_operations,
+            self.display,
+            self.event_handler,
             42)
 
     def side_effect(path):
