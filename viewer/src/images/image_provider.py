91410712c9016d14c154ad2ad24cf61ee84e08d3
David Sykes <david@sykes.org.uk>
2026-09-01 07:06:25 +0100

    ImageProvider loads and caches images

--- viewer/src/images/image_provider.py
+++ viewer/src/images/image_provider.py
@@ -2,8 +2,11 @@ class ImageProvider:
     def __init__(self, next_image_timer, image_loader):
         self._next_image_timer = next_image_timer
         self._image_loader = image_loader
+        self._cached_image = None
 
     def provide_image(self):
         path = self._next_image_timer.run_if_due()
-        image = self._image_loader.load_image(path)
-        return image
+        if path is None:
+            return self._cached_image
+        self._cached_image = self._image_loader.load_image(path)
+        return self._cached_image
--- viewer/tests/test_image_provider.py
+++ viewer/tests/test_image_provider.py
@@ -15,6 +15,18 @@ class ImageProviderTests(unittest.TestCase):
 
         self.assertEqual(image, 'image at new image')
 
+    def test_an_image_is_cached_until_the_next_image(self):
+        self.next_image_timer.run_if_due.return_value = 'image'
+        self.assertEqual(self.out.provide_image(), 'image at image')
+
+        self.next_image_timer.run_if_due.return_value = None
+        self.assertEqual(self.out.provide_image(), 'image at image')
+        self.assertEqual(self.out.provide_image(), 'image at image')
+        self.assertEqual(self.out.provide_image(), 'image at image')
+        
+        self.next_image_timer.run_if_due.return_value = 'new image'
+        self.assertEqual(self.out.provide_image(), 'image at new image')
+
     def setUp(self):
         self.next_image_timer = Mock(spec=ActionTimer)
         self.image_loader = Mock(spec=ImageLoader)
