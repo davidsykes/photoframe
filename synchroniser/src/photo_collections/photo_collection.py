434fdd6e6d08d96a2cddbaa6580ae834e2ddb666
David Sykes <david@sykes.org.uk>
2026-09-05 07:02:49 +0100

    Extend the photo collection definition to include filter tags

--- synchroniser/src/photo_collections/photo_collection.py
+++ synchroniser/src/photo_collections/photo_collection.py
@@ -2,5 +2,8 @@ class PhotoCollection:
     def __init__(self, photo_collection_raw):
         self.name = photo_collection_raw[0]
         self.url = photo_collection_raw[1]
-        self.filter_tags = photo_collection_raw[0]
+        if len(photo_collection_raw) > 2:
+            self.filter_tags = photo_collection_raw[2]
+        else:
+            self.filter_tags = photo_collection_raw[0]
 
--- synchroniser/tests/photo_collections/test_photo_collection.py
+++ synchroniser/tests/photo_collections/test_photo_collection.py
@@ -15,3 +15,7 @@ class TestPhotoCollection(unittest.TestCase):
     def test_filter_tags_is_the_name_if_only_two_values_are_provided(self):
         collection = PhotoCollection(['name', 'url'])
         self.assertEqual(collection.filter_tags, 'name')
+
+    def test_filter_tags_is_the_third_item_if_three_values_are_provided(self):
+        collection = PhotoCollection(['name', 'url', 'filter_tags'])
+        self.assertEqual(collection.filter_tags, 'filter_tags')
