e69b6cebd1c1e6a6151aebdedf0647a0c5543bc8
David Sykes <david@sykes.org.uk>
2026-09-20 06:38:38 +0100

    NextPhotoToShowCache tests pass

--- viewer/src/photos/sequential_photos_new/next_photo_to_show_cache.py
+++ viewer/src/photos/sequential_photos_new/next_photo_to_show_cache.py
@@ -1,6 +1,14 @@
 class NextPhotoToShowCache:
-    def __init__(self, next_photo_to_show_timer):
+    def __init__(self,
+                 next_photo_to_show_timer,
+                 photo_history):
         self._next_photo_to_show_timer = next_photo_to_show_timer
+        self._photo_history = photo_history
+        self._photo_cache = None
 
     def choose_next_photo(self):
-        raise 'choose_next_photo'
+        photo = self._next_photo_to_show_timer.run_if_due()
+        if photo is not None:
+            self._photo_cache = photo
+            self._photo_history.new_photo(photo)
+        return self._photo_cache
