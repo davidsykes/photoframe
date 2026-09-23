ed6ad44440b5f2c9b993089504a4c3167fd3395e
David Sykes <david@sykes.org.uk>
2026-09-23 07:58:53 +0100

    Digging deeper

--- viewer/src/monitoring/memory_monitor.py
+++ viewer/src/monitoring/memory_monitor.py
@@ -6,13 +6,24 @@ class MemoryMonitor:
         self._status_updater = status_updater
 
     def check_memory_usage(self):
-        command = 'pgrep -af python'
+        command = ['pgrep', '-af', 'python']
         try:
             output = self._subprocess_wrapper.run_return_stdout(command)
             output = output.splitlines()
-            self._status_updater.update_status('pgrep 0', 'Begin')
-            for o in range(len(output)):
-                self._status_updater.update_status(f'pgrep {o+1}', output[o])
-                print(f'SADSJALDJSLAJDLSJADJ {output[o]}')
+            if len(output) == 1:
+                self.process_pgrep_output(output[0])
+            else:
+                self.status_print(output)
         except Exception as e:
             self._status_updater.update_status('pgrep error', f'{command} {e}')
+
+    def status_print(self, lines):
+        self._status_updater.update_status('pgrep 0', 'Begin')
+        for o in range(len(lines)):
+            self._status_updater.update_status(f'pgrep {o+1}', lines[o])
+
+    def process_pgrep_output(self, prep_info):
+        pid = prep_info.split()[0]
+        command = ['ps', '-p', pid, '-o', 'pid,rss,vsz,%mem,cmd']
+        output = self._subprocess_wrapper.run_return_stdout(command)
+        output = output.splitlines()
